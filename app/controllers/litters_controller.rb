b3eb0184bca66fbce6ddf14ddefe556fdc380e59
github username <github email address>
2018-09-14 21:50:23 -0700

    stub out basic actions in litter controller

--- app/controllers/litters_controller.rb
+++ app/controllers/litters_controller.rb
@@ -1,7 +1,36 @@
 class LittersController < ApplicationController
+  before_action :find_litter, only: [:show, :edit, :update, :destroy]
 
   def index
     @litters = Litter.all.order("created_at DESC")
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def litter_params
+    params.require(:litter).premit(:name, :start_date, :end_date, :with_mom, :mom_name)
+  end
+
+  def find_litter
+    @litter = Litter.find(params[:id])
   end
 
 end
