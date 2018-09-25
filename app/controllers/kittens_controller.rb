8231b21069f107fe719721db75e9dab96eea2d06
github username <github email address>
2018-09-25 00:18:02 -0700

    new kitten form

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,11 +1,40 @@
 class KittensController < ApplicationController
   before_action :find_kitten, only: [:show, :edit, :update, :destroy]
 
+  def new
+    @litter = Litter.find(params[:litter_id])
+    @kitten = Kitten.new
+  end
+
+  def create
+    @kitten = Litter.kittens.new(kitten_params)
+    if @kitten.save
+      redirect_to litter_kitten_path(@litter, @kitten)
+    else
+      redirect_to litter_path(@litter)
+    end
+  end
+
   def show
     @litter = @kitten.litter
   end
 
   private
+
+  def kitten_params
+    params.require(:kitten).permit(
+      :id,
+      :user_id,
+      :litter_id,
+      :name,
+      :sex,
+      :color,
+      :image,
+      :image_cache,
+      :remove_image,
+      :_destroy
+    )
+  end
 
   def find_kitten
     @kitten = Kitten.find(params[:id])
