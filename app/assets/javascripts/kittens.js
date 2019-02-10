43d1588e2b57349d4c6439176866ad56245b8b8c
github username <github email address>
2019-02-09 18:38:36 -0800

    make Next Kitten button work and update url state

--- app/assets/javascripts/kittens.js
+++ app/assets/javascripts/kittens.js
@@ -4,7 +4,7 @@ $(function () {
 });
 
 function showKittens() {
-  $("button.next-kitten").on('click', function(e) {
+  $("body").on('click', 'button.next-kitten', function(e) {
     e.preventDefault();
     let pathArray = window.location.pathname.split('/');
     let litterId = pathArray[2];
@@ -16,6 +16,7 @@ function showKittens() {
         let newKitten = new Kit(kitten);
         let kittenHTML = newKitten.showKittenHTML();
         $('div.card.bg-light.showKitten').append(kittenHTML);
+        history.pushState(null, null, `${newKitten.id}`);
       });
   });
 }
@@ -29,11 +30,61 @@ class Kit {
     this.litter_id = obj.litter_id;
     this.image = obj.image;
     this.dob = obj.dob;
+    this.user = obj.user;
   }
 }
 
 Kit.prototype.showKittenHTML =  function() {
   return (`
-    <h1>${this.name}</h1>
+
+      <div class="row">
+
+        <div class="col-lg-6">
+            <img class="rounded m-2" src=${this.image.full.url}>
+        </div>
+
+        <div class="col-lg-6">
+          <div class="card-body">
+            <h2 class="card-title text-center ${this.sex ? 'boy' : 'girl'}">
+                ${this.name}
+            </h2>
+
+            <ul class="list-group list-group-flush mr-4 ml-4">
+              <li class="list-group-item">${this.color}</li>
+              <li class="list-group-item">${this.sex ? 'boy' : 'girl'}</li>
+              <li class="list-group-item">${this.dob}</li>
+              <li class="list-group-item">"age"</li>
+              <h6>
+                <li class="list-group-item text-muted">Fostered By:
+                <span class="font-weight-normal d-inline-block mb-0.5 ml-1">${this.user.username}</span>
+                </li>
+              </h6>
+            </ul>
+
+            <div class="text-center m-4">
+              <h4 class="litter-name"><a href="/litters/7">Z25 2018</a></h4>
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="row adjust-width">
+        <div class="col-xs-12 col-md-6 btn-margin">
+          <button class="btn btn-outline-primary btn-block prev-kitten"> &lt;&lt; Previous Kitten </button>
+        </div>
+
+        <div class="col-xs-12 col-md-6 btn-margin">
+         <button class="btn btn-outline-primary btn-block next-kitten"> Next Kitten &gt;&gt; </button>
+        </div>
+      </div>
+
+      <div class="row adjust-width">
+        <a class="btn btn-outline-primary btn-block" href="/litters/7">Back to Z25 2018 Litter</a>
+      </div>
+
+
+
   `);
 };
